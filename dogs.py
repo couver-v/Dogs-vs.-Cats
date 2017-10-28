f37ce0514a830504967ceb31781b48079fb27842
bertrand buffat <bertrand.buffat@epitech.eu>
2017-10-28 19:49:25 +0200

    added resize image to formatting functions

--- dogs.py
+++ dogs.py
@@ -5,17 +5,18 @@ import tensorflow as tf
 import random
 import os
 
-def get_pix(im):
-    """
-    takes image of file jpg
-    return array of pixel red + array of pixel green + array of pixel blue
-    """
-    pix = list(im.getdata())
-    width, height = im.size
-    pixArray = lambda pix, rank: [pix[y * width + x][rank] for x in range(width) for y in range(height)]
-    return pixArray(pix, 0) + pixArray(pix, 1) + pixArray(pix, 2)
+"""
+basic functions to get uniform inputs through batches
+"""
 
-def get_batch(size, dataset='train'):
+def get_pix(image, width, height):
+    """
+    return array of pixel red + array of pixel green + array of pixel blue, of size width * height * 3
+    """
+    image_resized = Image.open(image).resize((width, height))
+    return np.array(image_resized).T.flatten()
+
+def get_batch(size, width, height, dataset='train'):
     """
     return a random batch of size length
     each contains feature (pixelarray of r + g + b) and one hot vector target (0 for dog or 1 for cat) from train datatset
@@ -24,10 +25,8 @@ def get_batch(size, dataset='train'):
     targets = []
     for _ in range(size):
         type = 'dog' if random.randint(0,1) == 1 else 'cat'
-        nb = random.randint(0, 12400)
-        filename = '{type}.{nb}.jpg'.format(type=type, nb=nb)
-        im = Image.open('{dataset}/{filename}'.format(dataset=dataset, filename=filename))
-        features.append(get_pix(im))
+        filename = '{dataset}/{type}.{nb}.jpg'.format(dataset=dataset, type=type, nb=random.randint(0, 12400))
+        features.append(get_pix(filename, width, height))
         targets.append(np.eye(2)[0 if type == "dog" else 1])
     return features, targets
 
@@ -35,11 +34,17 @@ if __name__ == '__main__':
     if os.path.isdir("./train/") == False:
         print "miss folder for train data"
         exit (1)
-        
+
+    # hyper parameters
+
+    # images will all be resized to 224 * 224 pixels
+    width = 224
+    height = 224
+
     # initialize variables
 
     # train
-    for _ in range(50):
-        batch_xs, batch_ys = get_batch(20)
-
+    for _ in range(1):
+        batch_xs, batch_ys = get_batch(1, width, height)
+        
     # Test trained model
